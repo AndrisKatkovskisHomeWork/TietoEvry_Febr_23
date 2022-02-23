9252ec19e12f8a0033d16d7d114c42d4b9b8eef4
katkoand <crocr@inbox.lv>
2022-02-23 17:04:14 +0200

    test 2

---
+++ febr_23.py
@@ -0,0 +1,20 @@
+username = input("What is your username? ")
+print(f"Wow that is a nice name {username}")
+
+user_age = input("What is your username? ")
+print(f" {username}, how old are you?")
+print(f"Dear {username} , {user_age} is the best age?")
+
+user_age = int(user_age)
+years_to_100 = 100 - user_age
+year_when_100 = 2022 + years_to_100
+print(f" after {years_to_100} yoube 100 year old in {year_when_100}")
+user_age = int(user_age)
+user_age = int(user_age)
+
+user_age = int(user_age)
+
+user_age = int(user_age)
+
+
+
