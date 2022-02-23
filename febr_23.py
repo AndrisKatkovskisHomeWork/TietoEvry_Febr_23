21424bfec6f9d5676dc8f1e0de51bf48f7fad515
katkoand <crocr@inbox.lv>
2022-02-23 17:15:45 +0200

    test 2

--- febr_23.py
+++ febr_23.py
@@ -1,3 +1,11 @@
+import datetime
+
+currentDateTime = datetime.datetime.now()
+date = currentDateTime.date()
+year = date.strftime("%Y")
+print(f"Current Year -> {year}")
+year = int(year)
+
 username = input("What is your username? ")
 print(f"Wow that is a nice name {username}")
 
@@ -7,14 +15,7 @@ print(f"Dear {username} , {user_age} is the best age?")
 
 user_age = int(user_age)
 years_to_100 = 100 - user_age
-year_when_100 = 2022 + years_to_100
-print(f" after {years_to_100} yoube 100 year old in {year_when_100}")
-user_age = int(user_age)
-user_age = int(user_age)
-
-user_age = int(user_age)
-
-user_age = int(user_age)
-
+year_when_100 = year + years_to_100
+print(f" after {years_to_100} you be 100 year old in {year_when_100}")
 
 
