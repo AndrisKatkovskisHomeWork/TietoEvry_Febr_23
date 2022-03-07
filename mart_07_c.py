eed351b3cd75a80538ad9749d066627bbc4665a0
katkoand <crocr@inbox.lv>
2022-03-07 16:51:31 +0200

    Day 5 c uzdevums - uzlabojums, ja tekstam jāturpinās

--- mart_07_c.py
+++ mart_07_c.py
@@ -1,10 +1,11 @@
 user_text = input("please enter some text: ")
 
 position_not = user_text.find("not")
+position_bad = user_text.find("bad")
 
 print("position_not: ", position_not)
 
 if user_text.find("not") > 0 and user_text.find("bad") > 0:
-    user_text = f"{user_text[:position_not]}is good"
+    user_text = f"{user_text[:position_not]}is good{user_text[position_bad:]}"
 
 print("user_text: ", user_text)
